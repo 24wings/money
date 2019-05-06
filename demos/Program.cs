4984b08c234e1597f38496e9c6eebfe2cc519685
24wings <731502029@qq.com>
2019-05-06 18:15:49 +0800

    data

---
+++ demos/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using PInvoke;
+using static PInvoke.User32;
+namespace demos {
+    class Program {
+        static void Main (string[] args) {
+            MessageBox (IntPtr.Zero, "hello", "heheeda", MessageBoxOptions.MB_DEFBUTTON1);
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
